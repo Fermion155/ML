ffae0b3d13df815e2251fe21e877d40882a91fdf
Paul <pdrabuzinskis@gmail.com>
2020-10-14 04:52:07 +0300

    adding test

---
+++ test.py
@@ -0,0 +1,9 @@
+import time
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+print("Hello world")
+#time.sleep(5000)
